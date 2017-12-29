52a438aa5503c458c890e235d05d3490b2c64a48
xuanyuanxixi <1020301980@qq.com>
2017-12-29 10:16:12 +0800

    初始化3

--- scan/src/main/java/com/zbar/lib/CaptureActivity.java
+++ scan/src/main/java/com/zbar/lib/CaptureActivity.java
@@ -29,252 +29,256 @@ import com.zbar.lib.decode.InactivityTimer;
 
 import java.io.IOException;
 
+import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
+
 /**
- *二维码扫描
+ * 二维码扫描
  */
-public class CaptureActivity extends Activity implements Callback {
+public class CaptureActivity extends SwipeBackActivity implements Callback {
 
-	private CaptureActivityHandler handler;
-	private boolean hasSurface;
-	private InactivityTimer inactivityTimer;
-	private MediaPlayer mediaPlayer;
-	private boolean playBeep;
-	private static final float BEEP_VOLUME = 0.50f;
-	private boolean vibrate;
-	private int x = 0;
-	private int y = 0;
-	private int cropWidth = 0;
-	private int cropHeight = 0;
-	private RelativeLayout mContainer = null;
-	private RelativeLayout mCropLayout = null;
-	private boolean isNeedCapture = false;
-	
-	public boolean isNeedCapture() {
-		return isNeedCapture;
-	}
+    private CaptureActivityHandler handler;
+    private boolean hasSurface;
+    private InactivityTimer inactivityTimer;
+    private MediaPlayer mediaPlayer;
+    private boolean playBeep;
+    private static final float BEEP_VOLUME = 0.50f;
+    private boolean vibrate;
+    private int x = 0;
+    private int y = 0;
+    private int cropWidth = 0;
+    private int cropHeight = 0;
+    private RelativeLayout mContainer = null;
+    private RelativeLayout mCropLayout = null;
+    private boolean isNeedCapture = false;
 
-	public void setNeedCapture(boolean isNeedCapture) {
-		this.isNeedCapture = isNeedCapture;
-	}
+    public boolean isNeedCapture() {
+        return isNeedCapture;
+    }
 
-	public int getX() {
-		return x;
-	}
+    public void setNeedCapture(boolean isNeedCapture) {
+        this.isNeedCapture = isNeedCapture;
+    }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+    public int getX() {
+        return x;
+    }
 
-	public int getY() {
-		return y;
-	}
+    public void setX(int x) {
+        this.x = x;
+    }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+    public int getY() {
+        return y;
+    }
 
-	public int getCropWidth() {
-		return cropWidth;
-	}
+    public void setY(int y) {
+        this.y = y;
+    }
 
-	public void setCropWidth(int cropWidth) {
-		this.cropWidth = cropWidth;
-	}
+    public int getCropWidth() {
+        return cropWidth;
+    }
 
-	public int getCropHeight() {
-		return cropHeight;
-	}
+    public void setCropWidth(int cropWidth) {
+        this.cropWidth = cropWidth;
+    }
 
-	public void setCropHeight(int cropHeight) {
-		this.cropHeight = cropHeight;
-	}
+    public int getCropHeight() {
+        return cropHeight;
+    }
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    public void setCropHeight(int cropHeight) {
+        this.cropHeight = cropHeight;
+    }
 
-		setContentView(R.layout.activity_scanner);
-		// 初始化CameraManager
-		CameraManager.init(getApplication());
-		hasSurface = false;
-		inactivityTimer = new InactivityTimer(this);
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		mContainer = (RelativeLayout) findViewById(R.id.capture_containter);
-		mCropLayout = (RelativeLayout) findViewById(R.id.capture_crop_layout);
-		findViewById(R.id.loca_show_btncancle).setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				finish();
-			}
-		});
-		//获得屏幕的宽高
-		DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
-		int screenWith = displayMetrics.widthPixels;
-		LayoutParams params = (LayoutParams) mCropLayout.getLayoutParams();
-		params.width = screenWith*2/3;
-		params.height = screenWith*2/3;
-		mCropLayout.setLayoutParams(params);
-		ImageView mQrLineView = (ImageView) findViewById(R.id.capture_scan_line);
-		TranslateAnimation mAnimation = new TranslateAnimation(TranslateAnimation.ABSOLUTE, 0f, TranslateAnimation.ABSOLUTE, 0f,
-				TranslateAnimation.RELATIVE_TO_PARENT, 0f, TranslateAnimation.RELATIVE_TO_PARENT, 0.9f);
-		mAnimation.setDuration(1500);
-		mAnimation.setRepeatCount(-1);
-		mAnimation.setRepeatMode(Animation.REVERSE);
-		mAnimation.setInterpolator(new LinearInterpolator());
-		mQrLineView.setAnimation(mAnimation);
-	}
+        setContentView(R.layout.activity_scanner);
+        // 初始化CameraManager
+        CameraManager.init(getApplication());
+        hasSurface = false;
+        inactivityTimer = new InactivityTimer(this);
 
-	boolean flag = true;
+        mContainer = (RelativeLayout) findViewById(R.id.capture_containter);
+        mCropLayout = (RelativeLayout) findViewById(R.id.capture_crop_layout);
+        findViewById(R.id.loca_show_btncancle).setOnClickListener(new OnClickListener() {
 
-	protected void light() {
-		if (flag == true) {
-			flag = false;
-			// 打开
-			CameraManager.get().openLight();
-		} else {
-			flag = true;
-			// 关闭
-			CameraManager.get().offLight();
-		}
+            @Override
+            public void onClick(View arg0) {
+                finish();
+            }
+        });
+        //获得屏幕的宽高
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+        int screenWith = displayMetrics.widthPixels;
+        LayoutParams params = (LayoutParams) mCropLayout.getLayoutParams();
+        params.width = screenWith * 2 / 3;
+        params.height = screenWith * 2 / 3;
+        mCropLayout.setLayoutParams(params);
+        ImageView mQrLineView = (ImageView) findViewById(R.id.capture_scan_line);
+        TranslateAnimation mAnimation = new TranslateAnimation(TranslateAnimation.ABSOLUTE, 0f, TranslateAnimation.ABSOLUTE, 0f,
+                TranslateAnimation.RELATIVE_TO_PARENT, 0f, TranslateAnimation.RELATIVE_TO_PARENT, 0.9f);
+        mAnimation.setDuration(1500);
+        mAnimation.setRepeatCount(-1);
+        mAnimation.setRepeatMode(Animation.REVERSE);
+        mAnimation.setInterpolator(new LinearInterpolator());
+        mQrLineView.setAnimation(mAnimation);
+    }
 
-	}
+    boolean flag = true;
 
-	@SuppressWarnings("deprecation")
-	@Override
-	protected void onResume() {
-		super.onResume();
-		SurfaceView surfaceView = (SurfaceView) findViewById(R.id.capture_preview);
-		SurfaceHolder surfaceHolder = surfaceView.getHolder();
-		if (hasSurface) {
-			initCamera(surfaceHolder);
-		} else {
-			surfaceHolder.addCallback(this);
-			surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-		}
-		playBeep = true;
-		AudioManager audioService = (AudioManager) getSystemService(AUDIO_SERVICE);
-		if (audioService.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
-			playBeep = false;
-		}
-		initBeepSound();
-		vibrate = true;
-	}
+    protected void light() {
+        if (flag == true) {
+            flag = false;
+            // 打开
+            CameraManager.get().openLight();
+        } else {
+            flag = true;
+            // 关闭
+            CameraManager.get().offLight();
+        }
 
-	@Override
-	protected void onPause() {
-		super.onPause();
-		if (handler != null) {
-			handler.quitSynchronously();
-			handler = null;
-		}
-		CameraManager.get().closeDriver();
-	}
+    }
 
-	@Override
-	protected void onDestroy() {
-		inactivityTimer.shutdown();
-		super.onDestroy();
-	}
+    @SuppressWarnings("deprecation")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.capture_preview);
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        if (hasSurface) {
+            initCamera(surfaceHolder);
+        } else {
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+        playBeep = true;
+        AudioManager audioService = (AudioManager) getSystemService(AUDIO_SERVICE);
+        if (audioService.getRingerMode() != AudioManager.RINGER_MODE_NORMAL) {
+            playBeep = false;
+        }
+        initBeepSound();
+        vibrate = true;
+    }
 
-	public void handleDecode(String result) {
-		inactivityTimer.onActivity();
-		playBeepSoundAndVibrate();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        CameraManager.get().closeDriver();
+    }
 
-		Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
-		finish();
+    @Override
+    protected void onDestroy() {
+        inactivityTimer.shutdown();
+        super.onDestroy();
+    }
 
-	}
+    public void handleDecode(String result) {
+        inactivityTimer.onActivity();
+        playBeepSoundAndVibrate();
 
-	private void initCamera(SurfaceHolder surfaceHolder) {
-		try {
-			CameraManager.get().openDriver(surfaceHolder);
+        Toast.makeText(getApplicationContext(), result, Toast.LENGTH_SHORT).show();
+        finish();
 
-			Point point = CameraManager.get().getCameraResolution();
-			int width = point.y;
-			int height = point.x;
+    }
 
-			int x = mCropLayout.getLeft() * width / mContainer.getWidth();
-			int y = mCropLayout.getTop() * height / mContainer.getHeight();
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        try {
+            CameraManager.get().openDriver(surfaceHolder);
 
-			int cropWidth = mCropLayout.getWidth() * width / mContainer.getWidth();
-			int cropHeight = mCropLayout.getHeight() * height / mContainer.getHeight();
+            Point point = CameraManager.get().getCameraResolution();
+            int width = point.y;
+            int height = point.x;
 
-			setX(x);
-			setY(y);
-			setCropWidth(cropWidth);
-			setCropHeight(cropHeight);
-			setNeedCapture(true);
-			
+            int x = mCropLayout.getLeft() * width / mContainer.getWidth();
+            int y = mCropLayout.getTop() * height / mContainer.getHeight();
 
-		} catch (IOException ioe) {
-			return;
-		} catch (RuntimeException e) {
-			return;
-		}
-		if (handler == null) {
-			handler = new CaptureActivityHandler(CaptureActivity.this);
-		}
-	}
+            int cropWidth = mCropLayout.getWidth() * width / mContainer.getWidth();
+            int cropHeight = mCropLayout.getHeight() * height / mContainer.getHeight();
 
-	@Override
-	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            setX(x);
+            setY(y);
+            setCropWidth(cropWidth);
+            setCropHeight(cropHeight);
+            setNeedCapture(true);
 
-	}
 
-	@Override
-	public void surfaceCreated(SurfaceHolder holder) {
-		if (!hasSurface) {
-			hasSurface = true;
-			initCamera(holder);
-		}
-	}
+        } catch (IOException ioe) {
+            return;
+        } catch (RuntimeException e) {
+            return;
+        }
+        if (handler == null) {
+            handler = new CaptureActivityHandler(CaptureActivity.this);
+        }
+    }
 
-	@Override
-	public void surfaceDestroyed(SurfaceHolder holder) {
-		hasSurface = false;
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 
-	}
+    }
 
-	public Handler getHandler() {
-		return handler;
-	}
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (!hasSurface) {
+            hasSurface = true;
+            initCamera(holder);
+        }
+    }
 
-	private void initBeepSound() {
-		if (playBeep && mediaPlayer == null) {
-			setVolumeControlStream(AudioManager.STREAM_MUSIC);
-			mediaPlayer = new MediaPlayer();
-			mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-			mediaPlayer.setOnCompletionListener(beepListener);
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        hasSurface = false;
 
-			AssetFileDescriptor file = getResources().openRawResourceFd(R.raw.beep);
-			try {
-				mediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
-				file.close();
-				mediaPlayer.setVolume(BEEP_VOLUME, BEEP_VOLUME);
-				mediaPlayer.prepare();
-			} catch (IOException e) {
-				mediaPlayer = null;
-			}
-		}
-	}
+    }
 
-	private static final long VIBRATE_DURATION = 200L;
+    public Handler getHandler() {
+        return handler;
+    }
 
-	private void playBeepSoundAndVibrate() {
-		if (playBeep && mediaPlayer != null) {
-			mediaPlayer.start();
-		}
-		if (vibrate) {
-			Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-			vibrator.vibrate(VIBRATE_DURATION);
-		}
-	}
+    private void initBeepSound() {
+        if (playBeep && mediaPlayer == null) {
+            setVolumeControlStream(AudioManager.STREAM_MUSIC);
+            mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setOnCompletionListener(beepListener);
 
-	private final OnCompletionListener beepListener = new OnCompletionListener() {
-		public void onCompletion(MediaPlayer mediaPlayer) {
-			mediaPlayer.seekTo(0);
-		}
-	};
+            AssetFileDescriptor file = getResources().openRawResourceFd(R.raw.beep);
+            try {
+                mediaPlayer.setDataSource(file.getFileDescriptor(), file.getStartOffset(), file.getLength());
+                file.close();
+                mediaPlayer.setVolume(BEEP_VOLUME, BEEP_VOLUME);
+                mediaPlayer.prepare();
+            } catch (IOException e) {
+                mediaPlayer = null;
+            }
+        }
+    }
+
+    private static final long VIBRATE_DURATION = 200L;
+
+    private void playBeepSoundAndVibrate() {
+        if (playBeep && mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+        if (vibrate) {
+            Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+            vibrator.vibrate(VIBRATE_DURATION);
+        }
+    }
+
+    private final OnCompletionListener beepListener = new OnCompletionListener() {
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            mediaPlayer.seekTo(0);
+        }
+    };
 }
